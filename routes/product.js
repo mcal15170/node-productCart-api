a341063b698269069a8224b154649d9bacdb7646
Kaushik Dhameliya <dhameliyakaushik13@gmail.com>
2019-07-29 17:17:52 +0530

    set public directory

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var product = require('./../controller/ProductCOntroller');
-var multer = require('multer');
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/images/uploads')
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now()+ '.jpeg')
-    }
-});
-var upload = multer({storage: storage});
+
+var upload = require('./../Config/multer');
 
 /* GET home page. */
 router.post('/addproduct', upload.single('img'), async function(req, res, next) {
